342a740c5b740145e0d6af8dfb19afa561330e3f
zhanc100 <zhanc100@pascal.cas.mcmaster.ca>
2021-04-16 00:04:03 -0400

    fixed the conflict

--- counter.c
+++ counter.c
@@ -9,17 +9,24 @@
 #include <string.h>
 #include <ctype.h>
 
+
 #define COUNT_SIZE 26
+#define BUFFER_SIZE 1024
+
+int max(int count[]);
+int min(int count[]);
 
 int main()
 {
-  char buffer[1024], input, curchar;
+  char buffer[BUFFER_SIZE], input, curchar;
   int i = 0, count[COUNT_SIZE];
   int other = 0;
 
+
+
   // request and read in the string from the user
   printf("Enter text for analysis: ");
-  while ( (input = getchar()) != '\n' ) {
+  while ( ((input = getchar()) != '\n') && (i < (BUFFER_SIZE - 1))) {
     buffer[i++] = input;
   }
   buffer[i] = '\0';
@@ -36,7 +43,10 @@ int main()
   for (i = 0; i < strlen(buffer); i++) {
     curchar = toupper(buffer[i]);
     if (curchar >= 65 && curchar <= 90) count[curchar - 65]++;
+
     else other++;
+
+
   }
 
   // Create the letter analysis table
@@ -48,33 +58,53 @@ int main()
                                count[i],
                                (((float) count[i]) / strlen(buffer)) * 100);
   }
-  // Output the number of other characters
-  printf("%-10s%-15d%-15.2f\n","Other",
-                              other,
-                              (((float) count[i]) / strlen(buffer)) * 100);
+  printf("\nTotal spaces: %d\n", other);
+
 
   // Find the max and min occuring character in the string, in particular the
   // position in the count array of each character
-  int max, min, max_pos, min_pos;
-  max = min = count[0];
-  min_pos = max_pos = 0;
-  for (i = 0; i < COUNT_SIZE; i++) {
-    if (count[i] < min)
-    {
-      min_pos = i;
-      min = count[i];
-    }
+  int max_pos = max(count);
+  int min_pos = min(count);
+  
+  // Output the max and min occuring character, again using 65 as an offset to
+  // output the character character given ASCII A-Z range from 65-90
+  printf("\nMost frequently occuring character: %c\n", 
+         max_pos + 65);
+  printf("Least frequently occurring character: %c.\n", 
+         min_pos + 65);
+
+  return 0;
+}
+
+
+// Returns the position in array count of the associated letter that 
+// occurred the maximum number of times
+int max(int count[])
+{
+  int max = count[0];
+  int max_pos = 0;
+  for (int i = 0; i < 26; i++) {
     if (count[i] > max)
     {
       max_pos = i;
       max = count[i];
     }
-  }
+  }  
+  return max_pos;
+}
 
-  // Output the max and min occuring letter, again using 65 as an offset to
-  // output the character character given ASCII A-Z range from 65-90
-  printf("\nThe most frequently occurring letter is %c.\n", max_pos + 65);
-  printf("The least frequently occurring letter is %c.\n", min_pos + 65);
-
-  return 0;
+// Returns the position in array count of the associated letter that 
+// occurred the minimum number of times
+int min(int count[])
+{
+  int min = count[0];
+  int min_pos = 0;
+  for (int i = 0; i < 26; i++) {
+    if (count[i] < min)
+    {
+      min_pos = i;
+      min = count[i];
+    }
+  }  
+  return min_pos;
 }
